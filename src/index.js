1208e1a13fb868a6a805216e55969946d6068e3c
Kirill <bloodwingest@gmail.com>
2022-07-31 18:38:13 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let bitsArray = [];
+    for (let i = 0; i < expr.length; i+=10) {
+        bitsArray.push(expr.slice(i, i+10))
+    }
+    let changedBits = bitsArray.map((bits) => bits.slice(bits.search("1")));
+
+    let morseCode = [];
+
+    for (let i = 0; i < changedBits.length; i++) {
+        let morseArray = [];
+        for (let j = 0; j < changedBits[i].length; j+=2){
+            let pair = "";
+            pair = changedBits[i].slice(j, j+2);
+            switch(pair){
+                case '10':
+                    morseArray.push('.');
+                    break;
+                case '11':
+                    morseArray.push('-')
+                    break;
+                case '*':
+                    morseArray.push(' ')
+                    break;
+            }
+        }
+        morseCode.push(morseArray.join(""));
+    }
+
+    let convertedString = morseCode.map(function (morseSymbol) {
+        for (let key in MORSE_TABLE){
+            if(key === morseSymbol){
+                return MORSE_TABLE[key];
+            }
+        }
+        return " ";
+    }).join("");
+
+    return convertedString
 }
+
+
 
 module.exports = {
     decode
